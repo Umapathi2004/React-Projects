a5da686760aad9cd98f5a9f5699f4d81e56afbe1
Umapathi2004 <umapathi1014@gmail.com>
2024-07-10 11:40:29 +0530

    Add Index.css and Index.html and main.jsx

---
+++ Amount/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./index.css"
+import { Amount } from './Conponentes/Amount'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Amount/>
+  </React.StrictMode>
+)
